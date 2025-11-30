aa10f8ec729f17cc783552b2cd4ce0161d9ec7aa
ohhyeonseo <ohs4784@gmail.com>
2025-12-01 05:16:23 +0900

    Feat : 리액트에 실시간 송출

---
+++ AV1/logger.py
@@ -0,0 +1,21 @@
+from flask_sock import Sock
+
+class LogStreamer:
+  def __init__(self):
+    self.clients = []
+  
+  def register(self, ws):
+    self.clients.append(ws)
+    while True:
+      msg = ws.receive()
+      if msg is None:
+        break
+  
+  def write(self, text):
+    for ws in list(self.clients):
+      try:
+        ws.send(text)
+      except:
+        self.clients.remove(ws)
+
+log = LogStreamer()
