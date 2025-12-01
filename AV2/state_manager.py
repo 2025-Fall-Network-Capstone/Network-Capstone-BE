5ca57afef2f26d7690bb8746395c15ef60a8140f
ohhyeonseo <ohs4784@gmail.com>
2025-12-01 14:48:44 +0900

    Fix : 차량 상태 관리

--- AV2/state_manager.py
+++ AV2/state_manager.py
@@ -1,55 +1,36 @@
 class StateManager:
-    def __init__(self, role):
+    def __init__(self, role="AV2"):
         self.role = role
-        
         self.data = {
             "id": "AV2",
-            "speed": 35,              # 초기값
+            "speed": 35,
             "lane_change": False,
             "position": [0, 0],
             "direction": "left_turn",
-            "stage": 0,
-            #"ev_info": None,          # 관제에서 받는 EV 상태
-            #"from_ev": None,          # EV → AV2
+            "stage": 0
         }
 
-    # 관제가 stage 업데이트
     def update_stage(self, stage):
         self.data["stage"] = stage
         self.apply_stage_rules(stage)
 
-    # 단계별 동작 정의 (AV2 버전)
     def apply_stage_rules(self, stage):
-
         if stage == 0:
-            self.data["speed"] = 0
+            self.data["speed"] = 35
             self.data["lane_change"] = False
             self.data["direction"] = "left_turn"
-
         elif stage == 1:
-            self.data["speed"] = 20
-            self.data["direction"] = "STRAIGHT"
-
+            self.data["speed"] = 40
+            self.data["direction"] = "straight"
         elif stage == 2:
             self.data["lane_change"] = True
-            self.data["direction"] = "RIGHT"
-
+            self.data["direction"] = "right_turn"
         elif stage == 3:
-            self.data["speed"] = 10
-
+            self.data["speed"] = 20
         elif stage == 4:
             self.data["speed"] = 0
 
-    
-
-    def get_data(self):
+    def get(self):
         return self.data
 
-'''# EV → AV2
-    def update_from_ev(self, data):
-        self.data["from_ev"] = data
-
-
-    # 관제에서 EV info 전달
-    def update_ev_info(self, ev_info):
-        self.data["ev_info"] = ev_info'''
+state = StateManager()
