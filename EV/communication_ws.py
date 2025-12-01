ad40ec837c21d6250972ebeb8887f745f07162ea
ohhyeonseo <ohs4784@gmail.com>
2025-12-01 14:32:45 +0900

    Fix : ev -> 관제, AV로 state 전체 전송 ws 방식으로 서버 연결 후 전송

---
+++ EV/communication_ws.py
@@ -0,0 +1,39 @@
+import socketio
+from state_manager import state
+from config import CONTROL, AV1, AV2
+
+class CommunicationWS:
+    def __init__(self, state):
+        self.state = state
+        self.control_client = socketio.Client()
+        self.av1_client = socketio.Client()
+        self.av2_client = socketio.Client()
+        self.connect_all()
+
+    def connect_all(self):
+        try:
+            self.control_client.connect(f"http://{CONTROL}")
+            print("[EV] Connected to CONTROL")
+        except:
+            print("[EV] Failed to connect CONTROL")
+
+        try:
+            self.av1_client.connect(f"http://{AV1}")
+            print("[EV] Connected to AV1")
+        except:
+            print("[EV] Failed to connect AV1")
+
+        try:
+            self.av2_client.connect(f"http://{AV2}")
+            print("[EV] Connected to AV2")
+        except:
+            print("[EV] Failed to connect AV2")
+
+    def send_state(self):
+        data = self.state.get()
+        try:
+            self.control_client.emit("ev_state", data)
+            self.av1_client.emit("ev_state", data)
+            self.av2_client.emit("ev_state", data)
+        except:
+            pass
