74039954611f1f0aad5b508eda0535d514dc99cf
ohhyeonseo <ohs4784@gmail.com>
2025-12-06 05:29:31 +0900

    Fix : 소문자

--- EV/state_manager.py
+++ EV/state_manager.py
@@ -9,7 +9,7 @@ class StateManager:
             "speed": 0,
             "lane_change": False,
             "position": [6, 6],
-            "direction": "STRAIGHT",
+            "direction": "straight",
             "emergency": False,
             "stage": 0
         }
@@ -29,33 +29,33 @@ class StateManager:
         # 기본값 초기화
         self.data["speed"] = 0
         self.data["lane_change"] = False
-        self.data["direction"] = "STRAIGHT"
+        self.data["direction"] = "straight"
         self.data["position"] = [6, 6]
         self.data["emergency"] = False
 
         if stage == 0:
             self.data["speed"] = 80
             self.data["lane_change"] = False
-            self.data["direction"] = "STRAIGHT"
+            self.data["direction"] = "straight"
         elif stage == 1:
             self.data["position"] = [6,6]
             self.data["speed"] = 80
-            self.data["direction"] = "STRAIGHT"
+            self.data["direction"] = "straight"
             self.data["emergency"] = True
         elif stage == 2:
             self.data["position"] = [4,6]
             self.data["speed"] = 80
-            self.data["direction"] = "STRAIGHT"
+            self.data["direction"] = "straight"
             self.data["emergency"] = True
         elif stage == 3:
             self.data["position"] = [3,6]
             self.data["speed"] = 80
-            self.data["direction"] = "STRAIGHT"
+            self.data["direction"] = "straight"
             self.data["emergency"] = True
         elif stage == 4:
             self.data["position"] = [0,6]
             self.data["speed"] = 80
-            self.data["direction"] = "STRAIGHT"
+            self.data["direction"] = "straight"
             self.data["emergency"] = True
 
     def get(self):
