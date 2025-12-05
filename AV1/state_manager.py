a0e217d5d62584aa9dcde1ba19096329bb78c216
ohhyeonseo <ohs4784@gmail.com>
2025-12-05 23:49:59 +0900

    Fix : 데이터 수정

--- AV1/state_manager.py
+++ AV1/state_manager.py
@@ -19,6 +19,7 @@ class StateManager:
             self.apply_stage_rules(stage)
 
     def apply_stage_rules(self, stage):
+        
         if stage == 0:
             self.data["speed"] = 45
             self.data["lane_change"] = False
@@ -26,13 +27,13 @@ class StateManager:
 
         elif stage == 1:
             self.data["position"] = [4, 3]
-            self.data["speed"] = 45
+            self.data["speed"] = 35
             self.data["direction"] = "straight"
 
         elif stage == 2:
             self.data["position"] = [3, 3]
             self.data["lane_change"] = True
-            self.data["direction"] = "left_turn"
+            self.data["direction"] = "straight"
 
         elif stage == 3:
             self.data["lane_change"] = False
@@ -41,8 +42,8 @@ class StateManager:
             
         elif stage == 4:
             self.data["lane_change"] = True
-            self.data["position"] = [1, 6]
-            self.data["direction"] = "right_turn"
+            self.data["position"] = [2, 3]
+            self.data["direction"] = "straight"
 
     def get(self):
         with self.lock:
--- AV2/state_manager.py
+++ AV2/state_manager.py
@@ -21,20 +21,29 @@ class StateManager:
     def apply_stage_rules(self, stage):
         if stage == 0:
             self.data["speed"] = 45
+            self.data["lane_change"] = False
             self.data["direction"] = "straight"
+
         elif stage == 1:
             self.data["position"] = [4, 6]
             self.data["speed"] = 45
             self.data["direction"] = "straight"
+
         elif stage == 2:
             self.data["position"] = [2, 5]
-            self.data["speed"] = 35
+            self.data["lane_change"] = True
+            self.data["direction"] = "left_turn"
+
         elif stage == 3:
+            self.data["lane_change"] = False
             self.data["position"] = [2, 3]
-            self.data["speed"] = 40
+            self.data["direction"] = "straight"
+            
         elif stage == 4:
-            self.data["position"] = [2, 3]
-            self.data["speed"] = 45
+            self.data["lane_change"] = True
+            self.data["position"] = [1, 6]
+            self.data["direction"] = "right_turn"
+        
 
     def get(self):
         with self.lock:
